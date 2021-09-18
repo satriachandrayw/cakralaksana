8f47646be14ef34f59cf9991fc7b8cb5e51749e5
Satria Chandra <satriachandrayw@gmail.com>
2021-09-18 22:40:38 +0700

    chore: config heroku mysql server

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "cakralaksana";
-	$password = "P@ssw0rd123";
-	$database = "cakralaksana";
+	$host = "us-cdbr-east-04.cleardb.com";
+	$user = "bef7eb8e540614";
+	$password = "701f5dd0";
+	$database = "heroku_b62687d6962d44c";
 	
 	$connect = mysqli_connect($host,$user,$password,$database) or die (mysqli_error($connect));
 	//mysql_select_db($database) or die (mysql_error());
